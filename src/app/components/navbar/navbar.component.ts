e2b5b8414eab4700927c15868700272167bb54c3
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-06-29 04:02:27 -0500

    implementando componente navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isDropdownVisible = false;
 
+  categorias = [
+    'Stand up', 'Donación', 'Salud y bienestar', 'Tiendas', 'Conciertos',
+    'Entretenimiento', 'Fiestas', 'Seminarios y conferencias', 'Teatro',
+    'Viajes & Aventuras', 'Arte & Cultura', 'Cursos y talleres', 'Deportes',
+    'Comidas & Bebidas', 'Festivales', 'Tecnología', 'Niños', 'Ayuda Social',
+    'Cine'
+  ];
+
+  // Alterna el estado del dropdown
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  // Escucha los clics fuera del dropdown para cerrarlo
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event: Event) {
+    const dropdownMenu = document.getElementById("dropdownMenu");
+    const filterButton = document.getElementById("filterButton");
+
+    if (!event.target) return;
+    const target = event.target as HTMLElement;
+
+    if (!target.closest('.filter-button-categories') && !target.closest('.dropdown-menu')) {
+      if (dropdownMenu?.classList.contains('show')) {
+        dropdownMenu.classList.remove('show');
+        filterButton?.classList.remove("active");
+        this.isDropdownVisible = false;
+      }
+    }
+  }
 }
