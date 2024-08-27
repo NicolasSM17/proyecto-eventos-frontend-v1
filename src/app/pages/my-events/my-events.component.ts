3b40567cf7b71e1b530d4713d4899e9895adb580
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-08-27 03:14:07 -0500

    mostrando eventos del organizador desde la base de datos y añadiendo iconos en el componente my-events

--- src/app/pages/my-events/my-events.component.ts
+++ src/app/pages/my-events/my-events.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { Evento } from 'src/app/model/event.model';
+import { EventService } from 'src/app/services/event.service';
+import { ImageProcessingService } from 'src/app/services/image-processing.service';
 
 @Component({
   selector: 'app-my-events',
   templateUrl: './my-events.component.html',
   styleUrls: ['./my-events.component.css']
 })
-export class MyEventsComponent {
+export class MyEventsComponent implements OnInit{
+  organizadorId: number;
+  eventos: Evento[] = [];
 
+  constructor(private activatedRoute: ActivatedRoute, private eventoService: EventService,
+              private imageProcessingService: ImageProcessingService){}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(
+      params => {
+        this.organizadorId = +params['organizadorId'];
+        this.getEventosDelOrganizador();
+      }
+    );
+  }
+
+  getEventosDelOrganizador(){
+    this.eventoService.getEventoByIdOrganizador(this.organizadorId).pipe(
+      map(
+        (x: Evento[], i) => x.map(
+          (evento: Evento) => {
+            const [hours, minutes] = evento.hora.split(':');
+            evento.horaDate = new Date(0, 0, 0, +hours, +minutes);
+
+            return this.imageProcessingService.createImages(evento);
+          }
+        )
+      )
+    ).subscribe(
+      (response: Evento[]) => {
+        this.eventos = response;
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
 }
